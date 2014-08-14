9b1ea1fd76f0ecd3c000b0438a14b502673cb72c
Simon Kinslow <simonkinslow@gmail.com>
2014-08-14 09:45:11 +0100

    Ogg Files Versions, Tweaks To Test Sound Support
    Project failed in browsers that don't support MP3 sound. Added a basic callBack function to the sound_play function could be advanced to work with an object that unpacks the parameters like the sound_fade function

--- _assets/_js/Main.js
+++ _assets/_js/Main.js
@@ -5,12 +5,14 @@
 	
 	var soundEffects_pedal;
 	
+	var weatherFX = false;
+	
 	
 	$(document).ready( function(){ thisReady(); } );
 	
 	function thisReady()
 	{
-		sound_init();
+		sound_init(setup);
 	}
 	
 	function setup()
@@ -40,8 +42,6 @@
 		
 		
 		sound_play("level_bg_forest");
-		
-		whale_test();
 	
 		seaTest();
 	}
@@ -56,11 +56,14 @@
 	
 	/////////// SOUND
 	
-	function sound_init()
+	function sound_init(callBack)
 	{
 		soundEffects_pedal = {};
 		
-		if(!createjs.Sound.initializeDefaultPlugins())
+		var soundTest_support = createjs.Sound.initializeDefaultPlugins();
+		
+		
+		if(soundTest_support == false || soundTest_support == undefined || soundTest_support == null)
 		{
 			trace("NO_SOUND_SUPPORT");
 			
@@ -69,27 +72,35 @@
 			mainClick_init(null);
 		}
 		
-		else
+		if(soundTest_support == true)
 		{
+            trace("SOUND_SUPPORT");
+            
             soundEffects_pedal.main = {};
+            
+            if(callBack)
+            {
+	         	soundEffects_pedal.main.soundSetupCallBack = callBack;   
+            }
             
             soundEffects_pedal.main.fileCount = 0;
             
             soundEffects_pedal.main.assetsPath = "_assets/_sound/";
             
 		    soundEffects_pedal.main.manifest =	[
-		                							{src:"bg_forest.mp3", id:"BGM_BG_FOREST"},
-		                							{src:"bg_sea.mp3", id:"BGM_BG_SEA"},
-													{src:"fx_splash.mp3", id:"BGM_FX_SPLASH"},
-													{src:"fx_crow.mp3", id:"BGM_FX_CROW"},
-													{src:"fx_squeak.mp3", id:"BGM_FX_RABBIT"},
-													{src:"fx_moo.mp3", id:"BGM_FX_WHALE"}
+		                							{src:"bg_forest.ogg", id:"BGM_BG_FOREST"},
+													{src:"bg_sea.ogg", id:"BGM_BG_SEA"},
+													{src:"fx_splash.ogg", id:"BGM_FX_SPLASH"},
+													{src:"fx_crow.ogg", id:"BGM_FX_CROW"},
+													{src:"fx_squeak.ogg", id:"BGM_FX_RABBIT"},
+													{src:"fx_moo.ogg", id:"BGM_FX_WHALE"}
 												];
             
 			createjs.Sound.alternateExtensions = ["mp3"];	// add other extensions to try loading if the src file extension is not supported
 			createjs.Sound.addEventListener("fileload", createjs.proxy(sound_loaded, this)); // add an event listener for when load is completed
 			createjs.Sound.registerManifest(soundEffects_pedal.main.manifest, soundEffects_pedal.main.assetsPath);
 			
+			soundEffects_pedal.main.playCountMax = 6;
 			soundEffects_pedal.main.enterFrame_in = null;
 			soundEffects_pedal.main.enterFrame_out = null;
 			soundEffects_pedal.main.fadeValue = 0.01;	            
@@ -98,24 +109,33 @@
 	
 	function sound_loaded(event)
 	{
+		trace(event);
+		
 		soundEffects_pedal.main.fileCount ++;
 		
 		if(soundEffects_pedal.main.fileCount == soundEffects_pedal.main.manifest.length)
 		{
-			setup();
+			// setup();
 			
 			sound_refreshList();
 			
-			sound_list("level_bg_forest", {id: "BGM_BG_FOREST", loop: -1, volume: 1}, {paused:true, storeInstance: true});
-			sound_list("level_bg_sea", {id: "BGM_BG_SEA", loop: -1, volume: 0}, {paused:true, storeInstance: true});
+			sound_list("level_bg_forest", {id: "BGM_BG_FOREST", loop: -1, volume: 1}, {paused:true, storeInstance: true, singleChannel: true});
+			sound_list("level_bg_sea", {id: "BGM_BG_SEA", loop: -1, volume: 0}, {paused:true, storeInstance: true, singleChannel: true});
 			
-			sound_list("fx_splash", {id: "BGM_FX_SPLASH", loop: -1, volume: 0.08}, {paused:true, storeInstance: true});
-			sound_list("fx_crow", {id: "BGM_FX_CROW", loop: 0, volume: 1}, {paused:true, storeInstance: true});
-			sound_list("fx_rabbit", {id: "BGM_FX_RABBIT", loop: 0, volume: 0.06}, {paused:true, storeInstance: true});
-			sound_list("fx_whale", {id: "BGM_FX_WHALE", loop: 0, volume: 1}, {paused:true, storeInstance: true});
+			sound_list("fx_splash", {id: "BGM_FX_SPLASH", loop: -1, volume: 0.05}, {paused:true, storeInstance: true, singleChannel: true});
+			sound_list("fx_crow", {id: "BGM_FX_CROW", loop: 0, volume: 1}, {paused:true, storeInstance: true, singleChannel: false});
+			sound_list("fx_rabbit", {id: "BGM_FX_RABBIT", loop: 0, volume: 0.06}, {paused:true, storeInstance: true, singleChannel: false});
+			sound_list("fx_whale", {id: "BGM_FX_WHALE", loop: 0, volume: 1}, {paused:true, storeInstance: true, singleChannel: true});
 
 			// SAFETY - FLUSH
 			soundEffects_pedal.main.fileCount = 0;
+			
+			createjs.Sound.removeEventListener("fileload", createjs.proxy(sound_loaded, this));
+			
+			if(soundEffects_pedal.main.soundSetupCallBack)
+			{
+				soundEffects_pedal.main.soundSetupCallBack();	
+			}
 		}		
 	}
 	
@@ -162,6 +182,8 @@
 		{
 			_SOUND.exists = true;
 			
+			_SOUND.playCount = 0;
+			
 			if(_SOUND.options.paused)
 			{
 				instance.stop();
@@ -178,19 +200,29 @@
 		}		
 	}
 	
-	function sound_play(soundRef)
+	function sound_play(soundRef, callBack)
 	{
 		var _SOUND = soundEffects_pedal.soundList[soundRef];
 		
 		if(_SOUND != undefined)
 		{
-			if(_SOUND.instance.playState === "playFinished")
+			if(_SOUND.instance.playState === "playFinished" || !_SOUND.options.singleChannel && _SOUND.playCount < soundEffects_pedal.main.playCountMax)
 			{
 				_SOUND.instance = createjs.Sound.play(_SOUND.settings.id, createjs.Sound.INTERRUPT_NONE, 0, 0, _SOUND.settings.loop, _SOUND.settings.volume);
 				
 				_SOUND.instance.uniqueId = soundRef;
-
-				_SOUND.instance.addEventListener("complete", sound_end);				
+				
+				if(callBack)
+				{
+					_SOUND.onEndCallBack = callBack;
+				}
+				
+				_SOUND.playCount++;
+				
+				if(_SOUND.options.singleChannel || _SOUND.playCount >= soundEffects_pedal.main.playCountMax)
+				{
+					_SOUND.instance.addEventListener("complete", sound_end);	
+				}				
 			}
 		}
 	}
@@ -203,7 +235,16 @@
 		trace(_SOUND);
 		trace("?????????????");
 		
+		_SOUND.playCount = 0;
+		
 		_SOUND.instance.removeEventListener("complete", sound_end);
+		
+		if(_SOUND.onEndCallBack)
+		{
+			_SOUND.onEndCallBack();
+			
+			delete _SOUND.onEndCallBack;
+		}
 	}
 	
 	function sound_stop(soundRef)
@@ -226,119 +267,6 @@
 		}
 	}
 	
-	function sound_purge()
-	{
-		for(var soundOBJ in soundEffects_pedal.soundList)
-		{
-			sound_stop(soundEffects_pedal.soundList[soundOBJ].ref);
-		}
-		
-		sound_refreshList();		
-	}
-
-	
-	/////////// SOUND
-	
-	function whale_test()
-	{
-		trace("whale_test();");
-		
-		$("#whaleMain").addClass("tween-whaleSprite"); 
-		$("#whaleMain .whaleSprite").addClass("tween-waterAmbience");
-		
-		$(".tween-whaleSprite")[0].addEventListener("webkitAnimationStart", whale_animationStart, false);
-		$(".tween-whaleSprite")[0].addEventListener("animationstart", whale_animationStart, false);
-
-		$(".tween-whaleSprite")[0].addEventListener("webkitAnimationEnd", whale_animationEnd, false);
-		$(".tween-whaleSprite")[0].addEventListener("animationend", whale_animationEnd, false);
-	}
-	
-	function whale_animationStart(event)
-	{
-		$(".tween-whaleSprite")[0].removeEventListener("webkitAnimationStart", whale_animationStart, false);
-		$(".tween-whaleSprite")[0].removeEventListener("animationstart", whale_animationStart, false);
-		
-		
-		$("#whaleMain").removeClass("tween-whaleSpriteSafety");
-		
-		sound_play("fx_splash");
-	}
-	
-	function whale_animationEnd(event)
-	{
-		var exitFrame;
-		
-		var soundTest;
-		
-		// trace(event);
-		
-		$("#whaleMain").addClass("tween-whaleSpriteSafety");
-		$("#whaleMain").removeClass("tween-whaleSprite");
-		$("#whaleMain .whaleSprite").removeClass("tween-waterAmbience");
-		
-		
-		sound_stop("fx_splash");
-		
-		
-		exitFrame = setTimeout(whale_test, 1.5 * 1000);
-	}
-	
-	function test_rabbit(event)
-	{
-		trace("test_rabbit();");
-	
-		sound_play("fx_rabbit");
-	}
-	
-	function test_crow(event)
-	{
-		trace("test_crow();");
-	
-		sound_play("fx_crow");
-	}
-	
-	function test_whale(event)
-	{
-		trace("test_whale();");
-	
-		sound_play("fx_whale");
-	}
-	
-	function seaTest()
-	{
-		var s = setTimeout(seaTestRun, 2000);
-	}
-	
-	function seaTestRun()
-	{
-		var css_sea;
-		
-		css_sea = 	{	
-						"-webkit-transform" : "translateY(0)",
-						"transform" : "translateY(0)"
-					};
-		
-		$("#sea").css(css_sea);
-		
-		$(".waterEdge_fx").css("opacity", "1");
-	
-		$(".tween-sea")[0].addEventListener("webkitTransitionEnd", seaTestRun0, false);
-		$(".tween-sea")[0].addEventListener("transitionend", seaTestRun0, false);
-	
-	
-		sound_play("level_bg_sea");
-		
-		sound_fadeInit("level_bg_sea", 1, "IN", {call_funct: test_done, call_params: ["OH", "MY", "GOD"]});
-	}
-	
-	function seaTestRun0(event)
-	{
-		$(".tween-sea")[0].removeEventListener("webkitTransitionEnd", seaTestRun0, false);
-		$(".tween-sea")[0].removeEventListener("transitionend", seaTestRun0, false);
-	
-		sound_fadeInit("level_bg_forest", 0, "OUT", {call_funct: test_basic});
-	}
-	
 	function sound_fadeInit(soundRef, volTarget, fadeType, onEnd)
 	{
 		var _SOUND = soundEffects_pedal.soundList[soundRef]; 
@@ -347,11 +275,15 @@
 		{
 			if(fadeType === "IN")
 			{
+				// sound_fadeInit("sound_name", 1, "IN", {call_funct: end_function, call_params: ["parameter0", 1, object2]});
+				
 				soundEffects_pedal.main.enterFrame_in = setInterval(sound_fadeRun, 20, _SOUND.instance, volTarget, fadeType, soundRef, onEnd);
 			}
 			
 			if(fadeType === "OUT")
 			{
+				// sound_fadeInit("sound_name", 0, "OUT", {call_funct: end_function});
+				
 				soundEffects_pedal.main.enterFrame_out = setInterval(sound_fadeRun, 20, _SOUND.instance, volTarget, fadeType, soundRef, onEnd);
 			}			
 		}
@@ -386,6 +318,7 @@
 					
 					else
 					{
+						// BASIC FUNCTION
 						onEnd.call_funct();	
 					}
 				}
@@ -401,6 +334,8 @@
 			if(vol > volTarget)
 			{
 				soundInstance.setVolume(vol -= soundEffects_pedal.main.fadeValue);
+				
+				trace(vol);
 			}
 				
 			else
@@ -428,14 +363,150 @@
 		}		
 	}
 	
-	function test_done(x, y, z)
+	function sound_purge()
 	{
-		trace("test_done(); " + x + " " + y + " " + z); 
+		for(var soundOBJ in soundEffects_pedal.soundList)
+		{
+			sound_stop(soundEffects_pedal.soundList[soundOBJ].ref);
+		}
+		
+		sound_refreshList();		
+	}
+
+	
+	/////////// SOUND
+	
+	
+	
+	
+	/////////// CLICKS
+	
+	function test_rabbit(event)
+	{
+		sound_play("fx_rabbit");
 	}
 	
-	function test_basic()
+	function test_crow(event)
 	{
-		trace("BASIC!!!!!!!!!!!!!!!!!!!!!!!!!");
+		sound_play("fx_crow");
 	}
+	
+	function test_whale(event)
+	{
+		sound_play("fx_whale", whale_stageFX);
+		
+		if(!weatherFX)
+		{
+			weatherFX = true;
+			
+			$("#weather").css("opacity", "1");
+		}
+		
+	}
+	
+	/////////// CLICKS
+	
+	
+	
+	
+	/////////// TESTS
+	
+	function seaTest()
+	{
+		var s = setTimeout(seaTestRun, 2000);
+	}
+	
+	function seaTestRun()
+	{
+		var css_sea;
+		
+		css_sea = 	{	
+						"-webkit-transform" : "translateY(0)",
+						"transform" : "translateY(0)"
+					};
+		
+		$(".tween-sea")[0].addEventListener("webkitTransitionEnd", seaTestRun0, false);
+		$(".tween-sea")[0].addEventListener("transitionend", seaTestRun0, false);		
+		
+		$("#sea").css(css_sea);
+		
+		$(".waterEdge_fx").css("opacity", "1");
+	
+		sound_play("level_bg_sea");
+		
+		sound_fadeInit("level_bg_sea", 1, "IN");
+	}
+	
+	function seaTestRun0(event)
+	{
+		trace("END");
+		
+		$(".tween-sea")[0].removeEventListener("webkitTransitionEnd", seaTestRun0, false);
+		$(".tween-sea")[0].removeEventListener("transitionend", seaTestRun0, false);
+		
+		whale_test();		
+		
+		sound_fadeInit("level_bg_forest", 0, "OUT");
+	}
+	
+	
+	function whale_test()
+	{
+		trace("whale_test();");
+		
+		$("#whaleMain").addClass("tween-whaleSprite"); 
+		$("#whaleMain .whaleSprite").addClass("tween-waterAmbience");
+		
+		$(".tween-whaleSprite")[0].addEventListener("webkitAnimationStart", whale_animationStart, false);
+		$(".tween-whaleSprite")[0].addEventListener("animationstart", whale_animationStart, false);
+
+		$(".tween-whaleSprite")[0].addEventListener("webkitAnimationEnd", whale_animationEnd, false);
+		$(".tween-whaleSprite")[0].addEventListener("animationend", whale_animationEnd, false);
+	}
+	
+	function whale_stageFX()
+	{
+		$("#weather").css("opacity", "0");
+		
+		$(".tween-Weather")[0].addEventListener("webkitTransitionEnd", whale_stageFX_end, false);
+		$(".tween-Weather")[0].addEventListener("transitionend", whale_stageFX_end, false);
+	}
+	
+	function whale_stageFX_end(event)
+	{
+		$(".tween-Weather")[0].removeEventListener("webkitTransitionEnd", whale_stageFX_end, false);
+		$(".tween-Weather")[0].removeEventListener("transitionend", whale_stageFX_end, false);
+	
+		weatherFX = false;
+	}
+	
+	function whale_animationStart(event)
+	{
+		$(".tween-whaleSprite")[0].removeEventListener("webkitAnimationStart", whale_animationStart, false);
+		$(".tween-whaleSprite")[0].removeEventListener("animationstart", whale_animationStart, false);
+		
+		
+		$("#whaleMain").removeClass("tween-whaleSpriteSafety");
+		
+		sound_play("fx_splash");
+	}
+	
+	function whale_animationEnd(event)
+	{
+		var exitFrame;
+		
+		$("#whaleMain").addClass("tween-whaleSpriteSafety");
+		$("#whaleMain").removeClass("tween-whaleSprite");
+		$("#whaleMain .whaleSprite").removeClass("tween-waterAmbience");
+		
+		sound_fadeInit("fx_splash", 0, "OUT", {call_funct: whale_delay, call_params: [ Math.floor(Math.random() * 3) + 1 ]});
+	}
+	
+	function whale_delay(s)
+	{
+		var wd = setTimeout(whale_test, s * 1000);
+	}	
+	
+	/////////// TESTS
 	
 	
